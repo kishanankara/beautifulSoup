0a34505bacfe66c21c5a20f655da4c37d0663c5c
Aditya Kishan <akishan@buffalo.edu>
2018-02-17 21:35:36 -0500

    Welcome Page prototype skeleton

--- upliftme/src/App.js
+++ upliftme/src/App.js
@@ -1,31 +1,72 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 import Happy from './assets/happy.png';
 import Angry from './assets/angry.png';
 import Sad from './assets/sad.png';
+import Vid from './assets/vid.mov';
+import Chill from './assets/chill.png';
+import ReactPlayer from 'react-player';
+import './styles/style.css'
 
-
+class under_dev extends Component{
+  render(){
+    return (
+      <div className='under_dev'>
+          <p>Under Development</p>
+      </div>
+    );
+  }
+}
 
 class Moods extends Component{
   render(){
     return(
       <div class ="moods">
-          <img src={Happy} class ="happy" width="400" height="400"/>
-          <img src={Angry} class ="angry" width="400" height="400"/>
-          <img src={Sad} class ="sad" width="400" height="400"/>
+          <img src={Happy} class ="happy" width="100" height="100" hspace="70"/>
+          <img src={Angry} class ="angry" width="100" height="100" hspace="70"/>
+          <img src={Sad} class ="sad" width="100" height="100" hspace="70"/>
+          <img src={Chill} class ="chill" width="100" height="100" hspace="70"/>
       </div>
     );
+  }
+}
 
+class Header extends Component{
+  render() {
+    return (
+      <header class="v-header container">
+          <div class ="fullscreen-video-wrap">
+            <video src={Vid} class="video_player" autoplay="true" loop="true"/>
+          </div>
+          <div class="header-overlay"></div>
+          <div class="header-content">
+            <h1>upLiftMe</h1>
+            <p>How are you feeling today? </p>
+            <div class="spacer"></div>
+            <Moods/>
+          </div>
+          </header>
+    );
+  }
+}
+
+class Description extends Component{
+  render(){
+    return (
+      <div>
+      <p>Choose a mood and we will filter out a playlist for you! </p>
+      </div>
+    );
   }
 }
 
 class App extends Component {
   render() {
     return (
-      <div className="header">
-      <h1> upliftMe </h1>
-      <p> How are you feeling today? </p>
-      <Moods/>
+      <div>
+      <under_dev/>
+      <Header/>
+
       </div>
     );
   }
