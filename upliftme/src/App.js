d137778e5436bbd38eab27df82c1adbd588cd8e0
Aditya Kishan <akishan@buffalo.edu>
2018-02-19 17:14:49 -0500

    Fixed a few issues in the welcome page

--- upliftme/src/App.js
+++ upliftme/src/App.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 import Happy from './assets/happy.png';
 import Angry from './assets/angry.png';
+import HoverImage from "react-hover-image";
 import Sad from './assets/sad.png';
 import Vid from './assets/vid.mov';
 import Vidwebm from './assets/vid.webm';
@@ -9,6 +10,7 @@ import ReactVideoComponent from 'react-video-component';
 import Chill from './assets/chill.png';
 import ReactPlayer from 'react-player';
 import './styles/style.css'
+import Descimg from './assets/Running_2.jpg';
 
 class under_dev extends Component{
   render(){
@@ -24,10 +26,10 @@ class Moods extends Component{
   render(){
     return(
       <div class ="moods">
-          <img src={Happy} class ="happy" width="100" height="100" hspace="70"/>
-          <img src={Angry} class ="angry" width="100" height="100" hspace="70"/>
-          <img src={Sad} class ="sad" width="100" height="100" hspace="70"/>
-          <img src={Chill} class ="chill" width="100" height="100" hspace="70"/>
+          <img src={Happy}  class ="happy" width="100" height="100" hspace="70" onClick={() => window.location = 'http://localhost:8888/callback'} />
+          <img src={Angry} class ="angry" width="100" height="100" hspace="70"  onClick={() => window.location = 'http://localhost:8888/callback'}/>
+          <img src={Sad} class ="sad" width="100" height="100" hspace="70" onClick={() => window.location = 'http://localhost:8888/callback'}/>
+          <img src={Chill} class ="chill" width="100" height="100" hspace="70" onClick={() => window.location = 'http://localhost:8888/callback'}/>
       </div>
     );
   }
@@ -39,13 +41,13 @@ class Header extends Component{
       <header class="v-header container">
           <div class ="fullscreen-video-wrap">
               <video autoplay="true" play="true" loop="true" muted="true">
-                <source src={Vid} type="video/mp4"/>
-                <source src={Vidwebm} type="video/webm"/>
+                <source src={Vid} type="video/mp4"/> //Safari
+                <source src={Vidwebm} type="video/webm"/> //Firefox
               </video>
           </div>
           <div class="header-overlay"></div>
           <div class="header-content">
-            <h1>upLiftMe</h1>
+            <h1>moodMatch</h1>
             <p>How are you feeling today? </p>
             <div class="spacer"></div>
             <Moods/>
@@ -55,14 +57,18 @@ class Header extends Component{
   }
 }
 
-
-
 class Description extends Component{
   render(){
     return (
-      <div>
-      <p>Choose a mood and we will filter out a playlist for you! </p>
-      </div>
+      <header class="desc-header container">
+          <div class ="fullscreen-picture-wrap">
+              <img src={Descimg}/>
+          </div>
+          <div class="headerimg-overlay"></div>
+          <div class="header-desc-content">
+            <p>Choose a mood and we will create a playlist that matches your mood. </p>
+          </div>
+          </header>
     );
   }
 }
@@ -71,7 +77,6 @@ class App extends Component {
   render() {
     return (
       <div>
-      <under_dev/>
       <Header/>
       <Description/>
       </div>
