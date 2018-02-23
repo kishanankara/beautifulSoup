1db86bab0fd445ab356a358f1ce9b0871e7e4987
Aditya Kishan <akishan@buffalo.edu>
2018-02-23 15:19:15 -0500

    Integrated with backend

--- upliftme/src/App.js
+++ upliftme/src/App.js
@@ -6,14 +6,16 @@ import Logo from './assets/logo.png';
 import User from './assets/user.png';
 //import HoverImage from "react-hover-image";
 import Sad from './assets/sad.png';
+import queryString from 'query-string';
 import Vid from './assets/vid.mov';
 import Vidwebm from './assets/vid.webm';
 import ReactVideoComponent from 'react-video-component';
 import Chill from './assets/chill.png';
 import ReactPlayer from 'react-player';
-import './styles/style.css'
+import './styles/style.css';
 import Descimg from './assets/Running_2.jpg';
 // import * as script_DropDown from './assets/script_DropDown.js';
+
 
 class under_dev extends Component{
   render(){
@@ -27,13 +29,34 @@ class under_dev extends Component{
 
 
 class Moods extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      data: {}
+    }
+  }
+  componentExecute(){
+    let parsd = queryString.parse(window.location.search);
+    let accessToken = parsd.access_token;
+
+    if (!accessToken)
+      return;
+    fetch('https://api.spotify.com/v1/me',{
+      headers: {'Authorization': 'Bearer ' + accessToken}
+    }).then(response => response.json()).then(data => this.setState({
+      user:
+      {
+        name: data.display_name
+      }
+    }))
+  }
   render(){
     return(
       <div class ="moods">
-          <img src={Happy}  class ="happy" width="90" height="90" hspace="75" onClick={() => window.location = 'http://localhost:8888/callback'} />
-          <img src={Angry} class ="angry" width="90" height="90" hspace="75"  onClick={() => window.location = 'http://localhost:8888/callback'}/>
-          <img src={Sad} class ="sad" width="90" height="90" hspace="75" onClick={() => window.location = 'http://localhost:8888/callback'}/>
-          <img src={Chill} class ="chill" width="90" height="90" hspace="75" onClick={() => window.location = 'http://localhost:8888/callback'}/>
+          <img src={Happy}  class ="happy" width="90" height="90" hspace="75" onClick={() => queryString.parse(window.location.search).access_token? window.location = 'http://localhost:3000/callback': window.location='http://localhost:8888/login'} />
+          <img src={Angry} class ="angry" width="90" height="90" hspace="75"  onClick={() => queryString.parse(window.location.search).access_token? window.location = 'http://localhost:3000/callback': window.location='http://localhost:8888/login'}/>
+          <img src={Sad} class ="sad" width="90" height="90" hspace="75" onClick={() => queryString.parse(window.location.search).access_token? window.location = 'http://localhost:3000/callback': window.location='http://localhost:8888/login'}/>
+          <img src={Chill} class ="chill" width="90" height="90" hspace="75" onClick={() => queryString.parse(window.location.search).access_token? window.location = 'http://localhost:3000/callback': window.location='http://localhost:8888/login'}/>
       </div>
     );
   }
@@ -44,7 +67,7 @@ class LogoIcon extends Component{
   render(){
     return(
       <div class ="logoIcon">
-          <img src={Logo}  class ="logo" width="130" height="50" hspace="20"/>
+          <img src={Logo}  class ="logo" width="130" height="50" hspace="20" onClick={() => window.location='http://localhost:3000/'}/>
           </div>
     );
   }
@@ -111,14 +134,37 @@ class Header extends Component{
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      user_name:'Mr/Mrs. X',
+      playlist:{
+        song1: 'Hello,Adele',
+        song2: 'In the End, Linkin Park'
+      }
+    }
+  }
   render() {
     return (
+      <div className="App">
+      {window.location.href != 'http://localhost:3000/callback'?
       <div>
-      <Header/>
       <header class = "logo-header">
         <ReloadItSelf/>
         <Drop/>
       </header>
+      <Header/>
+      </div>:
+                  <div style={{'text-align': 'center','font-size': '50px', 'margin-top': '20px'}}>
+                  <header class = "logo-header">
+                    <ReloadItSelf/>
+                    <Drop/>
+                  </header>
+                <h1>{this.state.user_name}'s Playlist</h1>
+                <p> {this.state.playlist.song1} </p>
+                <p2> {this.state.playlist.song2} </p2>
+                </div>
+              }
       </div>
     );
   }
