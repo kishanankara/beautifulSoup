8790cf2d4c9432e5531c3b167c0369e4e475bbc7
kishanankara <akishan@buffalo.edu>
2018-03-04 17:30:03 -0500

    Added comments in the render method.

--- upliftme/src/js/components/Playlists.js
+++ upliftme/src/js/components/Playlists.js
@@ -22,10 +22,14 @@ class Playlists extends Component{
 
   render(){
 
-    /*WHAT DOES THIS DO?*/
+    //The data that is recieved and set to the state is in the form of an object with a bunch of different attributes.
+   
     return(
       <div>
-          {this.state.data?
+          {this.state.data?  //This line checks if there is any data. Remember component did mount runs only after 
+           //the component renders the first time. Once that happens the method is called and the component re renders.
+           //Once there is a state variable called data the component goes over a map function (look up map for react) and then 
+           //populates the data on in the component. React re renders the component every time data changes.
               <div><p>The data is here : <br/></p><div> {this.state.data.playlists.items.map(m => <img src={m.images[0].url}/>)}</div></div>:
             <p1>Loading ... </p1>}
 
