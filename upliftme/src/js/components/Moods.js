2bc7d83598ca30d9493986e3a562a4465b87f609
Aditya Kishan <akishan@buffalo.edu>
2018-05-04 11:25:39 -0400

    Fixed the font

--- upliftme/src/js/components/Moods.js
+++ upliftme/src/js/components/Moods.js
@@ -9,6 +9,7 @@ import ReactTooltip from 'react-tooltip';
 
 //**This is the Moods component consists of four different moods to choose from upon a click
 //redirects the user to a backend server which processes the request to go on the playlist.**
+var happy_message = "Hello User! We are glad that you are \n feeling happy today. Click this emoji\n for some Happy songs!";
 
 class Moods extends Component{
   constructor(props){
@@ -21,14 +22,26 @@ class Moods extends Component{
     return(
       <div className ="moods">
           <span className = 'first_half'>
-            <img src={Happy} data-tip="Songs to bop around to" fontSize="15px" className ="happy" width="85" height="85" onClick={()=>window.location=frontendUri+'/Happy'} />
-            <img src={Angry} data-tip="Songs to shake your first with" fontSize="15px" className ="angry" width="85" height="85"  onClick={() =>window.location=frontendUri+'/Angry'}/>
+            <img src={Happy} data-tip data-for='happy-mood' className ="happy" width="85" height="85" onClick={()=>window.location=frontendUri+'/Happy'} />
+            <img src={Angry} data-tip data-for="angry-mood" className ="angry" width="85" height="85"  onClick={() =>window.location=frontendUri+'/Angry'}/>
           </span>
           <span className = 'second_half'>
-            <img src={Sad} data-tip="Songs to pluck at your heart chords" fontSize="15px" className ="sad" width="85" height="85" onClick={() => window.location=frontendUri+'/Sad'}/>
-            <img src={Chill} data-tip="Songs to relax to" fontSize="15px" className ="chill" width="85" height="85" onClick={() => window.location=frontendUri+'/Chill'}/>
+            <img src={Sad} data-tip data-for="sad-mood" className ="sad" width="85" height="85" onClick={() => window.location=frontendUri+'/Sad'}/>
+            <img src={Chill} data-tip data-for="chill-mood" className ="chill" width="85" height="85" onClick={() => window.location=frontendUri+'/Chill'}/>
           </span>
+          <ReactTooltip id='happy-mood' aria-haspopup='true' effect='solid' place='bottom'>
+          <p>Songs to bop around to</p>
+          </ReactTooltip>
+          <ReactTooltip id='angry-mood' aria-haspopup='true' effect='solid' place='bottom'>
+          <p>Songs to shake your first to</p>
 
+          </ReactTooltip>
+          <ReactTooltip id='sad-mood' aria-haspopup='true' effect='solid' place='bottom'>
+          <p>Songs to pluck at your heart chords</p>
+          </ReactTooltip>
+          <ReactTooltip id='chill-mood' aria-haspopup='true' effect='solid' place='bottom'>
+          <p>Songs to relax to</p>
+          </ReactTooltip>
       </div>
     );
   }
